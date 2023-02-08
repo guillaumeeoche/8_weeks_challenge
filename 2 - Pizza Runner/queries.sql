9eaf9d02c01c4c92de914ea44fcbfb39034aee65
guillaumeeoche <guillaume.eoche@gmail.com>
2023-02-08 13:04:07 +0100

    ✨ Add queries file

---
+++ 2 - Pizza Runner/queries.sql
@@ -0,0 +1,18 @@
+--schema init 
+
+DROP SCHEMA IF EXISTS pizza_runner CASCADE;
+CREATE SCHEMA pizza_runner; 
+
+CREATE TABLE pizza_runner.runners (
+    runner_id INT, 
+    registration_date TIMESTAMP
+); 
+
+INSERT INTO pizza_runner.runners (runner_id, registration_date)
+VALUES 
+(1, '2021-01-01'), 
+(2, '2021-01-03'), 
+(3, '2021-01-08'), 
+(4, '2021-01-15'); 
+
+
